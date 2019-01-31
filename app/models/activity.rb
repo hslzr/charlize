26adaed793514d0a5672e76e56e7cd93ae85dfd5
Salazar <h.salazar509@gmail.com>
2019-01-31 00:24:05 -0600

    Adds error when names are blank or dates do not match

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,16 @@ class Activity < ApplicationRecord
   belongs_to :event
 
   has_rich_text :description
+
+  # Validations
+  validates :name, :starts_at, :ends_at, presence: true
+  validate :end_date_cannot_be_before_start_date
+
+  private
+
+  def end_date_cannot_be_before_start_date
+    return if ends_at > starts_at
+
+    errors.add(:ends_at, 'can\'t be before start date')
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,16 @@ class Event < ApplicationRecord
 
   # Attachments
   has_one_attached :cover_image
+
+  # Validations
+  validates :name, :starts_at, :ends_at, presence: true
+  validate :end_date_cannot_be_before_start_date
+
+  private
+
+  def end_date_cannot_be_before_start_date
+    return if ends_at > starts_at
+
+    errors.add(:ends_at, 'can\'t be before start date')
+  end
 end
