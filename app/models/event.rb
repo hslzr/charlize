6521de74138bda49bc8227ccde2f835cea73b975
Salazar <h.salazar509@gmail.com>
2019-01-31 19:14:42 -0600

    Minor fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,6 +9,13 @@ class Event < ApplicationRecord
   validates :name, :starts_at, :ends_at, presence: true
   validate :end_date_cannot_be_before_start_date
 
+  # Scopes
+  scope :active_now, -> { where('starts_at <= NOW() AND ends_at >= NOW()') }
+
+  def active?
+    starts_at < DateTime.now && ends_at > DateTime.now
+  end
+
   private
 
   def end_date_cannot_be_before_start_date
