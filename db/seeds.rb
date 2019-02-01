b79e1fba13bc2868855a31efdd5fe96d1ded37c8
Salazar <h.salazar509@gmail.com>
2019-01-31 23:35:27 -0600

    Add single user for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin_user = User.create(
+  name: 'Santi',
+  last_name: 'Balmes',
+  password: 'demodemo',
+  password_confirmation: 'demodemo',
+  email: 'h.salazar509@gmail.com'
+)
