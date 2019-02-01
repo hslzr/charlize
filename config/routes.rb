18c52e16ef9ead9eb1307a398e016baac71302a0
Salazar <h.salazar509@gmail.com>
2019-01-31 20:15:31 -0600

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :events do
     resources :activities, except: [:index]
   end
+
+  resources :users
 
   root to: 'events#index'
 end
