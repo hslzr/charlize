0f04954f80770369025303be9530c444f3cf0657
Salazar <h.salazar509@gmail.com>
2019-02-11 01:08:07 -0600

    Minor updates, adds feather helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def feather(name, options = {})
+    icon_name = name
+    width     = options[:width] || 16
+    height    = options[:height] || 16
+    classes   = options[:classes] || []
+    classes  += ['v-mid']
+
+    content_tag :i, nil,
+                    width: width,
+                    height: height,
+                    class: classes,
+                    data: { feather: icon_name }
+  end
 end
